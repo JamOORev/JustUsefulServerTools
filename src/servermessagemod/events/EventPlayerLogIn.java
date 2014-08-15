48fb10b31089bbdca309d1adc26bc728453247b7
James Mills <ThePugPug89@gmail.com>
2014-08-15 19:17:53 +1000

    Added formatting for the messages

--- src/servermessagemod/events/EventPlayerLogIn.java
+++ src/servermessagemod/events/EventPlayerLogIn.java
@@ -3,6 +3,7 @@ package servermessagemod.events;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ChatComponentText;
 import servermessagemod.ConfigHandler;
+import servermessagemod.TextHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 
@@ -11,6 +12,35 @@ public class EventPlayerLogIn {
 	public static void onPlayerLoggedIn(PlayerLoggedInEvent event) {
 		System.out.println("BACON");
 		EntityPlayer player = event.player;
-		player.addChatComponentMessage(new ChatComponentText(ConfigHandler.logInMessage + player.getDisplayName() + "!"));
+		String message = ConfigHandler.logInMessage;
+		switch(ConfigHandler.colour) {
+		case "black": message = TextHelper.BLACK + message; break;
+		case "blue": message = TextHelper.BLUE + message; break;
+		case "green": message = TextHelper.GREEN + message; break;
+		case "teal": message = TextHelper.TEAL + message; break;
+		case "red": message = TextHelper.RED + message; break;
+		case "purple": message = TextHelper.PURPLE + message; break;
+		case "orange": message = TextHelper.ORANGE + message; break;
+		case "light gray": message = TextHelper.LIGHT_GRAY + message; break;
+		case "gray": message = TextHelper.GRAY + message; break;
+		case "light blue": message = TextHelper.LIGHT_BLUE + message; break;
+		case "bright green": message = TextHelper.BRIGHT_GREEN + message; break;
+		case "bright blue": message = TextHelper.BRIGHT_BLUE + message; break;
+		case "light red": message = TextHelper.LIGHT_RED + message; break;
+		case "pink": message = TextHelper.PINK + message; break;
+		case "yellow": message = TextHelper.YELLOW + message; break;
+		case "white": message = TextHelper.WHITE + message; break;
+		}
+		
+		switch(ConfigHandler.format) {
+		case "obsfucated": message = TextHelper.OBFUSCATED + message; break;
+		case "bold": message = TextHelper.BOLD + message; break;
+		case "strikethrough": message = TextHelper.STRIKETHROUGH + message; break;
+		case "underline": message = TextHelper.UNDERLINE + message; break;
+		case "italic": message = TextHelper.ITALIC + message; break;
+		case "end": message = TextHelper.END + message; break;
+		}
+		
+		player.addChatComponentMessage(new ChatComponentText(message + ", " + player.getDisplayName()));
 	}
 }
